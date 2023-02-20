f07c7b9e4dcc351e2205934a7166afd8934792cd
dongarehari <donhari541@gmail.com>
2023-02-21 01:52:35 +0530

    Bind with backend

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import './App.css';
 import { Component } from 'react';
 import ListContacts from './ListContacts.js'
+import * as ContactsAPI from './utils/ContactsAPIs'
 
 
 class App extends Component{
   state = {
     contacts: [
-      {
-        "id": "ajay",
-        "name": "Ajay Sathe",
-        "email": "ajay.sathe@awesome.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "niks",
-        "name": "Nikhil Sathe",
-        "email": "nikhil.sathe@awesome.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "vijay",
-        "name": "Vijay Sathe",
-        "email": "vijay.sathe@awesome.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      }
     ]
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) =>{
+      this.setState({ contacts })
+    })
   }
 
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
   
   render() {
