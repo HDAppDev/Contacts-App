2ffa034712672ee3e542f63cd6009feea9b7e8d2
dongarehari <donhari541@gmail.com>
2023-02-20 16:15:01 +0530

    state to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 import { Component } from 'react';
+import { ListContacts} from './ListContacts'
+
 
 class App extends Component{
+  state = {
+    contacts: [
+      {
+        "id": "ajay",
+        "name": "Ajay Sathe",
+        "email": "ajay.sathe@awesome.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "ajay",
+        "name": "Ajay Sathe",
+        "email": "ajay.sathe@awesome.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "ajay",
+        "name": "Ajay Sathe",
+        "email": "ajay.sathe@awesome.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      }
+    ]
+  }
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     )
   }
