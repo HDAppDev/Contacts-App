31357f9f18515801f164d1d5e4e2b1ea0ea352c0
dongarehari <donhari541@gmail.com>
2023-02-23 01:57:37 +0530

    Fixes some stuff!

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,12 @@
 import React, { Component} from 'react'
+import { Link } from 'react-router-dom'
 
 class CreateContact extends Component {
     render() {
         return (
-            <div>Create Contact!</div>
+            <div>
+                <link className='close-create-contaact' to="/">Close</link>
+            </div>
         )
     }
 }
